5f3eaf91d648e988362416b41df8b32c47a9fa4e
BradHick <bradleyhrm@gmail.com>
2021-04-20 16:54:45 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "88ab2d426a25fad3be41";
+/******/ 	var hotCurrentHash = "1344c79e8e11401e1c53";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
